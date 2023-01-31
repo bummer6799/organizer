275c65429d527ee6d49eb74a82b8e4441d38a642
bummer6799 <108254959+bummer6799@users.noreply.github.com>
2023-01-31 22:23:14 +0800

    1.2

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 import os
 import shutil
 
 
 def select_folder():
+    global folder_path
     folder_path = filedialog.askdirectory()
-    return folder_path
 
 
-def organize_folder(folder_path):
+def organize_folder():
+    if not folder_path:
+        messagebox.showerror("Error", "Please select a folder first")
+        return
+
+    if not messagebox.askokcancel("Organize Folder", "Are you sure you want to organize the selected folder?"):
+        return
+
     files = os.listdir(folder_path)
     file_types = set()
 
@@ -27,15 +35,18 @@ def organize_folder(folder_path):
         file_type = os.path.splitext(file)[1][1:]
         if file_type:
             shutil.move(os.path.join(folder_path, file), os.path.join(folder_path, file_type + 's', file))
+    messagebox.showinfo("Organize Folder", "The selected folder has been successfully organized")
 
 
 root = tk.Tk()
 root.title("File Organizer")
 
+folder_path = None
+
 select_folder_button = tk.Button(text="Select Folder", command=select_folder)
 select_folder_button.pack()
 
-organize_folder_button = tk.Button(text="Organize Folder", command=lambda: organize_folder(select_folder()))
+organize_folder_button = tk.Button(text="Organize Folder", command=organize_folder)
 organize_folder_button.pack()
 
 root.mainloop()
